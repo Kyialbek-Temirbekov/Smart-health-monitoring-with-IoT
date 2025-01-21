cf9bfb2ee9b012e47f7e340c4d2c81b8ec30744b
Кыялбек Темирбеков <28112002akn@gmail.com>
2025-01-21 10:02:11 +0600

    updated customer service

--- src/main/java/kg/edu/manas/cloud/api/CustomerController.java
+++ src/main/java/kg/edu/manas/cloud/api/CustomerController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.edu.manas.cloud.data.record.CreateCustomerRecord;
+import kg.edu.manas.cloud.data.record.CustomerRecord;
 import kg.edu.manas.cloud.data.record.OtpRecord;
 import kg.edu.manas.cloud.service.CustomerService;
 import lombok.RequiredArgsConstructor;
@@ -34,7 +35,7 @@ public class CustomerController {
     @Operation(summary = "logged in user data")
     @Parameter(name = "Authorization", in = ParameterIn.HEADER, example = "Basic <encoded_credentials>")
     @GetMapping
-    public ResponseEntity<CustomerDto> getCustomer() {
+    public ResponseEntity<CustomerRecord> getCustomer() {
         return new ResponseEntity<>(customerService.getCustomer(), HttpStatus.OK);
     }
     @Operation(summary = "delete user")
--- src/main/java/kg/edu/manas/cloud/service/CustomerService.java
+++ src/main/java/kg/edu/manas/cloud/service/CustomerService.java
@@ -1,12 +1,21 @@
 package kg.edu.manas.cloud.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import kg.edu.manas.cloud.data.record.CreateCustomerRecord;
 import kg.edu.manas.cloud.data.record.CustomerRecord;
+import kg.edu.manas.cloud.data.record.OtpRecord;
 import kg.edu.manas.cloud.entity.Customer;
 import kg.edu.manas.cloud.entity.Otp;
+import kg.edu.manas.cloud.exception.ConflictException;
 import kg.edu.manas.cloud.repository.CustomerRepository;
+import kg.edu.manas.cloud.repository.OtpRepository;
 import kg.edu.manas.cloud.util.NumericTokenGenerator;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,29 +26,30 @@ import java.util.concurrent.CompletableFuture;
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final OtpRepository otpRepository;
     private final PasswordEncoder passwordEncoder;
-    private final MessageService messageService;
 
     private static final String REGISTRATION_OTP_SUB = "One time password for registration";
     private static final long OTP_EXPIRY_HOUR = 3;
     private static final int OTP_LENGTH = 4;
 
-    public CustomerService(CustomerRepository customerRepository, PasswordEncoder passwordEncoder, MessageService messageService, CustomerReportService customerReportService, ExcelReportService reportService) {
+    public CustomerService(CustomerRepository customerRepository, OtpRepository otpRepository, PasswordEncoder passwordEncoder) {
         this.customerRepository = customerRepository;
+        this.otpRepository = otpRepository;
         this.passwordEncoder = passwordEncoder;
-        this.messageService = messageService;
     }
 
     @Transactional
     public void createCustomer(CreateCustomerRecord createCustomerRecord) {
-        Optional<Customer> optionalCustomer = customerRepository.findByUsername(createCustomerRecord.getEmail());
+        Optional<Customer> optionalCustomer = customerRepository.findByUsername(createCustomerRecord.username());
 
         if(optionalCustomer.isPresent() && optionalCustomer.get().isEnabled()) {
-            throw new ConflictException("User already exist: " + createCustomerRecord.getEmail());
+            throw new ConflictException("User already exist: " + createCustomerRecord.username());
         } else {
             String otpValue = NumericTokenGenerator.generateToken(OTP_LENGTH);
             CompletableFuture.runAsync(() ->
-                    messageService.sendMessage(new MessageDto(createCustomerRecord.getEmail(), REGISTRATION_OTP_SUB, otpValue))
+//                    messageService.sendMessage(new MessageDto(createCustomerRecord.getEmail(), REGISTRATION_OTP_SUB, otpValue))
+                            System.out.println("Sending otp: " + otpValue)
             );
             if(optionalCustomer.isPresent()) {
                 Customer customer = optionalCustomer.get();
@@ -48,7 +58,8 @@ public class CustomerService {
                 customer.setName(createCustomerRecord.name());
                 customer.setBirthDate(createCustomerRecord.birthDate());
 
-                Otp otp = optionalCustomer.get().getOtp();
+                /*Otp otp = optionalCustomer.get().getOtp();*/
+                Otp otp = otpRepository.findById(optionalCustomer.get().getId()).orElseThrow(EntityNotFoundException::new);
                 otp.setValue(otpValue);
                 otp.setExpiryTime(LocalDateTime.now().plusHours(OTP_EXPIRY_HOUR));
             } else {
@@ -56,29 +67,32 @@ public class CustomerService {
                 Otp otp = Otp.builder()
                         .value(otpValue)
                         .expiryTime(LocalDateTime.now().plusHours(OTP_EXPIRY_HOUR)).build();
-                customer.setOtp(otp);
+                /*customer.setOtp(otp);*/
                 otp.setCustomer(customer);
                 customerRepository.save(customer);
+                otpRepository.save(otp);
             }
         }
     }
 
     @Transactional
-    public void confirmEmail(OtpDto otpDto) {
-        Optional<Customer> optionalCustomer = customerRepository.findByUsername(otpDto.getEmail());
+    public void confirmEmail(OtpRecord otpRecord) {
+        Optional<Customer> optionalCustomer = customerRepository.findByUsername(otpRecord.email());
         if(optionalCustomer.isPresent()) {
             Customer customer = optionalCustomer.get();
-            if(customer.getOtp().isExpired()) {
-                throw new CredentialsExpiredException(ExceptionMessages.OTP_EXPIRED);
+            Otp otp = otpRepository.findById(customer.getId()).orElseThrow(EntityNotFoundException::new);
+            if(otp.isExpired()) {
+                throw new CredentialsExpiredException("One time password expired");
             }
-            else if(/*customer.getOtp().getValue().*/"0000".equals(otpDto.getOtp())) {
-                customer.setOtp(null);
+            else if(/*customer.getOtp().getValue().*/"0000".equals(otpRecord.otp())) {
+                /*customer.setOtp(null);*/
+                otpRepository.deleteById(customer.getId());
                 customer.setEnabled(true);
             } else {
-                throw new BadCredentialsException(ExceptionMessages.INVALID_OTP);
+                throw new BadCredentialsException("One time password is invalid");
             }
         } else {
-            throw new EntityNotFoundException(ExceptionMessages.USER_NOT_FOUND_BY_EMAIL + ":" + otpDto.getEmail());
+            throw new EntityNotFoundException("User not found by email: " + otpRecord.email());
         }
     }
 
@@ -89,7 +103,7 @@ public class CustomerService {
     public CustomerRecord getLoggedInUser() {
         String username = getPrincipal().getUsername();
         return customerRepository.findByUsername(username, CustomerRecord.class).orElseThrow(
-                () -> new EntityNotFoundException(ExceptionMessages.USER_NOT_FOUND_BY_EMAIL)
+                () -> new EntityNotFoundException("User not found by email")
         );
     }
 
