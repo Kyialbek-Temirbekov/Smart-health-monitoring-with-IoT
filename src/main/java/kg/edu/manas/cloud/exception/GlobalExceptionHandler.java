9b3b1160e6a77af3a2a9ff6e2f19db802bfa96a1
Кыялбек Темирбеков <28112002akn@gmail.com>
2025-01-23 17:34:32 +0600

    added exception handling

--- src/main/java/kg/edu/manas/cloud/exception/GlobalExceptionHandler.java
+++ src/main/java/kg/edu/manas/cloud/exception/GlobalExceptionHandler.java
@@ -1,14 +1,46 @@
 package kg.edu.manas.cloud.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import kg.edu.manas.cloud.model.data.record.ErrorResponseRecord;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.security.auth.login.CredentialExpiredException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<ErrorResponseRecord> handleException(ConflictException ex, WebRequest request) {
+        return new ResponseEntity<>(
+                new ErrorResponseRecord(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponseRecord> handleException(EntityNotFoundException ex, WebRequest request) {
+        return new ResponseEntity<>(
+                new ErrorResponseRecord(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+    @ExceptionHandler(CredentialExpiredException.class)
+    public ResponseEntity<ErrorResponseRecord> handleException(CredentialExpiredException ex, WebRequest request) {
+        return new ResponseEntity<>(
+                new ErrorResponseRecord(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponseRecord> handleException(AuthenticationException ex, WebRequest request) {
+        return new ResponseEntity<>(
+                new ErrorResponseRecord(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponseRecord> handleException(RuntimeException ex, WebRequest request) {
         return new ResponseEntity<>(
@@ -16,4 +48,5 @@ public class GlobalExceptionHandler {
                 HttpStatus.INTERNAL_SERVER_ERROR
         );
     }
+
 }
